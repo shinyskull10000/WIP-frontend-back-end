5866189d1abc0f562e2395dad8c423b4c9a37e1b
usc-TDC012 <148849366+shinyskull10000@users.noreply.github.com>
2025-08-26 21:18:01 +1000

    12
    1

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Let's say this is our "stored information"
+stored_data = "who?"
+
+@app.route("/")
+def index():
+    return render_template("index.html", data=stored_data)
+
+#if __name__ == "__main__":
+    # Render needs host="0.0.0.0"
+    #app.run(host="0.0.0.0", port=5000, debug=True)
